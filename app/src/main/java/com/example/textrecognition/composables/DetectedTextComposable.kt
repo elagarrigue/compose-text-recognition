749a04ca1b99d90776ca47ba130982d7e61c25b4
Emmanuel <elagarriguelazarte@doximity.com>
2023-01-03 17:12:19 -0300

    Clean up

--- app/src/main/java/com/example/textrecognition/composables/DetectedTextComposable.kt
+++ app/src/main/java/com/example/textrecognition/composables/DetectedTextComposable.kt
@@ -1,12 +1,9 @@
 package com.example.textrecognition.composables
 
 import android.graphics.Rect
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,16 +13,15 @@ import com.example.textrecognition.imageanalizer.DetectedTextBlocks
 
 @Composable
 fun DetectedText(textElement: DetectedTextBlocks.DetectedText, onTextClicked: (String) -> Unit) {
-    Box(
+    Canvas(
         modifier = Modifier
             .detectedTextSize(textElement.rect)
             .clip(RoundedCornerShape(size = 12.dp))
-            .background(Color.Yellow.copy(alpha = 0.5f))
             .clickable {
                 onTextClicked(textElement.text)
             }
     ) {
-        Text(modifier = Modifier.fillMaxSize(), text = "")
+        drawRect(Color.Yellow.copy(alpha = 0.5f))
     }
 }
 
